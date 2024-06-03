c5d767c4072a184d45d0400445d1354390da3fb1
kaos <61769776+Dev-Kaos@users.noreply.github.com>
2024-06-03 16:14:59 -0500

    docs(Configuration): :memo: Inicializa el repositorio

---
+++ src/main/java/com/app/baseone/BaseoneApplication.java
@@ -0,0 +1,13 @@
+package com.app.baseone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseoneApplication.class, args);
+	}
+
+}
